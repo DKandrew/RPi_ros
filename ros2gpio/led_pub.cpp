6ca539072ed5654f3cdbc969f2f0287c3ccedf69
Qinru <qinruli2@illinois.edu>
2016-06-20 17:18:32 -0500

    four LED can blink in different rate

--- ros2gpio/led_pub.cpp
+++ ros2gpio/led_pub.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <stdio.h>
 #include <geometry_msgs/Twist.h>
+#include <ros2gpio/led.h>  //include led.msg file 
 
 using namespace std;
 
@@ -9,14 +10,70 @@ int main(int argc, char **argv) {
 	ros::init(argc, argv, "led_pub");
 	ros::NodeHandle nh;
 	//Create a publisher object
-	ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("led/cmd_vel", 1000);
+	ros::Publisher pub = nh.advertise<ros2gpio::led>("led/control", 1000);
 	//Frequency Control
-	ros::Rate rate(2);
+	ros::Rate rate(8);
+	//Ask user for frequency
+	int freq1, freq2, freq3, freq4;
+	cout << "how fast do you want the first led to blink? " << endl;
+	cin >> freq1;
+	cout << "how fast do you want the second led to blink? " << endl;
+	cin >> freq2;
+	cout << "how fast do you want the third led to blink? " << endl;
+	cin >> freq3;
+	cout << "how fast do you want the forth led to blink? " << endl;
+	cin >> freq4;
+	int cntr1, cntr2, cntr3, cntr4;
+	cntr1=0;cntr2=0;cntr3=0;cntr4=0;
+	//Signal
+	int high=1; int low=0;
+	//Main
+	while(ros::ok()){
+		//Create led control message
+		ros2gpio::led msg;
+		if(cntr1%freq1==0){
+			msg.LED1 = low;
+			cntr1=0; 		//Clear control counter
+		}else{
+			msg.LED1 = high;
+		}
+		if(cntr2%freq2==0){
+			msg.LED2 = low;
+			cntr2=0; 		//Clear control counter
+		}else{
+			msg.LED2 = high;
+		}
+		if(cntr3%freq3==0){
+			msg.LED3 = low;
+			cntr3=0; 		//Clear control counter
+		}else{
+			msg.LED3 = high;
+		}
+		if(cntr4%freq4==0){
+			msg.LED4 = low;
+			cntr4=0; 		//Clear control counter
+		}else{
+			msg.LED4 = high;
+		}
+		//Update control counter
+		cntr1++;
+		cntr2++;
+		cntr3++;
+		cntr4++;
+		//Publish the message.
+		pub.publish(msg);
+		//Send a message to rosout with details
+		ROS_INFO_STREAM("Sending message of LED blinking command");
+		//Wait
+		rate.sleep(); 
+	}
+}	
+	
 	//Ask for flashing rate
-	cout << "How fast you want LED to blink?" << endl;
+	/*cout << "How fast you want LED to blink?" << endl;
 	int freq;
 	cin >> freq;
-	
+
 	int i = 1;
 	while(ros::ok()){
 		//Create message and fill this out
@@ -35,9 +92,8 @@ int main(int argc, char **argv) {
 		//Wait
 		rate.sleep(); 
 	}
-	
-	
-}
+	*/
+
 
 //Trash
 /*
--- ros2gpio/led_sub.cpp
+++ ros2gpio/led_sub.cpp
@@ -1,20 +1,47 @@
 #include <ros/ros.h>
 #include <stdio.h>
 #include <geometry_msgs/Twist.h>
+#include <ros2gpio/led.h>  //include led message 
 #include "wiringPi.h"
 
 using namespace std;
 
-int LED_ON;
+int LED1_ON;
+int LED2_ON;
+int LED3_ON;
+int LED4_ON;
 
 //Callback function
-void messageHandler(const geometry_msgs::Twist &msg){
-	double one = 1.0;
+void messageHandler(const ros2gpio::led &msg){
+	//double one = 1.0;
 	//double zero = 0.0;
-	if(msg.linear.x == one){
-		LED_ON = 1;
-	}else{
-		LED_ON = 0;
+	switch(msg.LED1) {
+		case 1:
+			LED1_ON = 1;
+			break;
+		default:
+			LED1_ON = 0;
+	}
+	switch(msg.LED2) {
+		case 1:
+			LED2_ON = 1;
+			break;
+		default:
+			LED2_ON = 0;
+	}
+	switch(msg.LED3) {
+		case 1:
+			LED3_ON = 1;
+			break;
+		default:
+			LED3_ON = 0;
+	}
+	switch(msg.LED4) {
+		case 1:
+			LED4_ON = 1;
+			break;
+		default:
+			LED4_ON = 0;
 	}
 }
 
@@ -29,13 +56,13 @@ int main(int argc, char **argv){
 	pinMode(2, OUTPUT);
 	pinMode(3, OUTPUT);
 	//Create a subscriber
-	ros::Subscriber sub = nh.subscribe("led/cmd_vel", 1000, &messageHandler);
+	ros::Subscriber sub = nh.subscribe("led/control", 1000, &messageHandler);
 	while(ros::ok()){
 		ros::spinOnce();
-		if(LED_ON == 1)
-			digitalWrite(7, HIGH);
-		else if (LED_ON == 0)
-			digitalWrite(7, LOW);
-		cout << "Received and processed msg" << endl;
+		digitalWrite(7, LED1_ON);
+		digitalWrite(0, LED2_ON);
+		digitalWrite(2, LED3_ON);
+		digitalWrite(3, LED4_ON);
+		//cout << "Received and processed msg" << endl;
 	}
 }
